973fb3ca610e252304195702d9602621225d28d8
Sebastian Arca <sebastianarca@gmail.com>
2025-03-07 16:18:24 -0300

    ECR Count Images - full tested

--- aws/ecr_count_images.sh
+++ aws/ecr_count_images.sh
@@ -13,31 +13,39 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+echo "" > ecr_count_images.csv.log
+
 echo -e "\n\n Counting images in each repository..."
+echo "\n\n Counting images in each repository" >> ecr_count_images.csv.log
 for repository in $repositories; do
     echo "Counting images in repository: $repository"
-    images_count=$(aws --profile $AWS_PROFILE ecr describe-images --repository-name $repository  --query 'length(imageDetails[*])' --output text)
+    echo "\n\n $repository" >> ecr_count_images.csv.log
+    images_count=$(aws --profile $AWS_PROFILE ecr describe-images --repository-name $repository  --query 'sum(to_array(length(imageDetails[*])))' --output json)
+    echo ":$images_count" >> ecr_count_images.csv.log
     ecr_count_images[$repository]=$images_count
 done
 
 echo -e "\n\n Getting latest image pushed date in each repository..."
+echo "\n\n Getting latest image pushed date in each repository" >> ecr_count_images.csv.log
 for repository in $repositories; do
     echo "Getting latest image pushed date in repository: $repository"
+    
+    echo "\n\n $repository" >> ecr_count_images.csv.log
     # Get timestamp and clean microseconds + adjust timezone
-    latest_pushed_image=$(aws --profile $AWS_PROFILE ecr describe-images --repository-name $repository \
-        --query 'sort_by(imageDetails,&imagePushedAt)[-1].imagePushedAt' --output text | \
-        sed 's/\(\.[0-9]\{6\}\)\([-+][0-9]\{2\}\):\([0-9]\{2\}\)/\1\2\3/')
+    latest_pushed_image=$(aws --profile $AWS_PROFILE ecr describe-images --repository-name $repository --query 'sort(to_array(sort_by(imageDetails,&imagePushedAt)[-1].imagePushedAt))[-1]' --output json)
+    echo ":$latest_pushed_image" >> ecr_count_images.csv.log
     
     ecr_date_latest_image_pushed["$repository"]="N/A"
     
     if [ -n "$latest_pushed_image" ]; then
-        formatted_date=$(date -d "$latest_pushed_image" +"%Y-%m-%d %H:%M:%S" 2>/dev/null)
-        if [ $? -eq 0 ]; then
-            ecr_date_latest_image_pushed["$repository"]=$formatted_date
-        else
-            echo "Fail: Invalid date format for $repository. Using raw value."
-            ecr_date_latest_image_pushed["$repository"]=$latest_pushed_image
-        fi
+        ecr_date_latest_image_pushed["$repository"]=$latest_pushed_image
+        # formatted_date=$(date -d "$(echo "$latest_pushed_image" | sed 's/\(\.[0-9]\{6\}\)\([-+][0-9]\{2\}\):\([0-9]\{2\}\)/\1\2\3/' )" +"%Y-%m-%d %H:%M:%S" 2>/dev/null)
+        # if [ $? -eq 0 ]; then
+        #     ecr_date_latest_image_pushed["$repository"]=$formatted_date
+        # else
+        #     echo "Fail: Invalid date format for $repository. Using raw value."
+        #     ecr_date_latest_image_pushed["$repository"]=$latest_pushed_image
+        # fi
     fi
 done
 
